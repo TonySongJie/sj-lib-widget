e55b60a4e70b49d90d0a98e97e0acac538b38656
冷锋sj记忆 <songjie_xuan@126.com>
2020-03-13 12:43:06 +0800

    增加自定义消息红点

--- app/src/main/java/com/sj/lib/test/MainActivity.kt
+++ app/src/main/java/com/sj/lib/test/MainActivity.kt
@@ -3,6 +3,7 @@ package com.sj.lib.test
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import com.xuan.widget.badgelibrary.BadgeViewPro
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,5 +17,9 @@ class MainActivity : AppCompatActivity() {
         appEtvText.initWidth(windowManager.defaultDisplay.width)
         appEtvText.initMaxLines(2)
         appEtvText.setCloseText("测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试")
+
+        val badgeViewPro = BadgeViewPro(this)
+        badgeViewPro.text = "100"
+        badgeViewPro.setTargetView(topToolbarTest)
     }
 }
